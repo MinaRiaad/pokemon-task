8faf69d6e18f1ec2ee728dd8d149bd4b7a990446
MinaRiaad <59794569+MinaRiaad@users.noreply.github.com>
2026-07-20 17:51:11 +0300

    chore: adding third party tools and preparing error boundary and loading handler

---
+++ src/ErrorBoundary.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+
+type Props = {
+  children: React.ReactNode;
+  fallback?: React.ReactNode;
+};
+
+type State = {
+  hasError: boolean;
+};
+
+class ErrorBoundary extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: unknown, info: unknown) {
+    console.error(error, info);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return this.props.fallback ?? null;
+    }
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode, Suspense } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { QueryClientProvider } from "@tanstack/react-query";
+import queryClient from "./queryClient";
+import ErrorBoundary from "./ErrorBoundary";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <ErrorBoundary fallback={<div>Something went wrong</div>}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <App />
+        </Suspense>
+      </ErrorBoundary>
+    </QueryClientProvider>
   </StrictMode>,
-)
+);
